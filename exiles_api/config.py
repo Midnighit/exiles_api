8c406d745571ff9f5c0795876f8c7f3fd1c51923
Midnighit <alexeuler.ffm@gmail.com>
2020-06-13 19:12:12 +0200

    Removed config. exiles_api expects config to be in app folder now.

--- exiles_api/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GAME_DB_URI = "sqlite:///" + os.getenv('SAVED_DIR_PATH') + "/game.db"
-USERS_DB_URI = "sqlite:///" + os.getenv('SAVED_DIR_PATH') + "/supplemental.db"
-ECHO = False
