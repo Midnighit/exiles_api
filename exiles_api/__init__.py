50fb58d9bed9bc524f13d1e6a59c17f792b94e9d
unknown <alexeuler.ffm@gmail.com>
2020-07-29 22:37:07 +0200

    v1.7.7 and pushing again AFTER saving the files...

--- exiles_api/__init__.py
+++ exiles_api/__init__.py
@@ -354,7 +354,7 @@ class Characters(GameBase, Owner):
         for id in characters:
             guild = session.query(Characters).get(id).guild
             if guild and guild.members == 0:
-                session.delete(char.guild)
+                session.delete(char.guild) 
         session.query(ActorPosition).filter(ActorPosition.id.in_(characters)).delete(synchronize_session='fetch')
         session.query(CharacterStats).filter(CharacterStats.char_id.in_(characters)).delete(synchronize_session='fetch')
         session.query(ItemInventory).filter(ItemInventory.owner_id.in_(characters)).delete(synchronize_session='fetch')
