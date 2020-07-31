4875d9308d4c04e7b18c7d30235a5403c6c336d5
unknown <alexeuler.ffm@gmail.com>
2020-07-31 02:42:20 +0200

    v1.7.9 see last commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='exiles_api',
-    version='1.7.8',
+    version='1.7.9',
     description='API to handle common tasks with the game.db and supplemental.db',
     url='git@github.com:Midnighit/exiles_api.git',
     author='Midnight',
