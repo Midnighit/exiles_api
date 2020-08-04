68badbfdaca9acfc50e68666bf603e2b8230aa97
unknown <alexeuler.ffm@gmail.com>
2020-08-04 11:03:08 +0200

    v1.7.12 on Characters.remove, check for existing guild members after removal of chars

--- exiles_api/__init__.py
+++ exiles_api/__init__.py
@@ -371,10 +371,6 @@ class Characters(GameBase, Owner):
     def remove(characters, autocommit=True):
         if not isinstance(characters, (dict, list, set, tuple)):
             characters = (characters,)
-        for id in characters:
-            guild = session.query(Characters).get(id).guild
-            if guild and guild.members == 0:
-                session.delete(char.guild)
         session.query(ActorPosition).filter(ActorPosition.id.in_(characters)).delete(synchronize_session='fetch')
         session.query(CharacterStats).filter(CharacterStats.char_id.in_(characters)).delete(synchronize_session='fetch')
         session.query(ItemInventory).filter(ItemInventory.owner_id.in_(characters)).delete(synchronize_session='fetch')
@@ -382,6 +378,10 @@ class Characters(GameBase, Owner):
         session.query(Properties).filter(Properties.object_id.in_(characters)).delete(synchronize_session='fetch')
         session.query(Purgescores).filter(Purgescores.purge_id.in_(characters)).delete(synchronize_session='fetch')
         session.query(Characters).filter(Characters.id.in_(characters)).delete(synchronize_session='fetch')
+        for id in characters:
+            guild = session.query(Characters).get(id).guild
+            if guild and guild.members == 0:
+                session.delete(char.guild)
         if autocommit:
             session.commit()
 
